cd0c1a7f370e5f1620aaa45175e162ff7bfa00e9
harichselvam <harichselvamc@gmail.com>
2023-06-04 07:07:54 +0530

    Update Intern.py

--- Intern.py
+++ Intern.py
@@ -4222,11 +4222,235 @@
 
 
 
+# import streamlit as st
+# from PIL import Image, ImageDraw, ImageFont
+# import io
+# import json
+# import base64
+
+# def add_image_overlay(images, image_data_list):
+#     images_with_overlay = []
+
+#     for image, image_data in zip(images, image_data_list):
+#         img = Image.open(io.BytesIO(image))
+#         overlay = Image.new('RGBA', img.size)
+
+#         image_name = image_data['image_name']
+#         font_size = image_data['font_size']
+#         position = image_data['position']
+#         text_color = image_data['text_color']
+#         font = ImageFont.truetype('arial.ttf', font_size)
+
+#         if position == 'bottom-left':
+#             x = 10
+#             y = img.height - font_size - 10
+#         elif position == 'bottom-right':
+#             text_width, _ = font.getsize(image_name)
+#             x = img.width - text_width - 10
+#             y = img.height - font_size - 10
+#         else:
+#             x = 10
+#             y = 10
+
+#         draw = ImageDraw.Draw(overlay)
+#         draw.text((x, y), image_name, font=font, fill=text_color)
+
+#         img_with_overlay = Image.alpha_composite(img.convert('RGBA'), overlay)
+#         images_with_overlay.append(img_with_overlay)
+
+#     return images_with_overlay
+
+# def resize_image(image, size):
+#     width, height = size
+#     return image.resize((width, height), resample=Image.LANCZOS)
+
+# def store_json_data(json_data):
+#     with open('history.json', 'a') as file:
+#         file.write(json_data + '\n')
+
+# def load_json_data():
+#     data = []
+#     with open('history.json', 'r') as file:
+#         for line in file:
+#             data.append(json.loads(line))
+#     return data
+
+# def save_to_history(data):
+#     image_name = data['image_name']
+#     font_size = data['font_size']
+#     position = data['position']
+#     text_color = data['text_color']
+#     altered_size = data.get('altered_size', None)
+
+#     image_data = {
+#         'image_name': image_name,
+#         'font_size': font_size,
+#         'position': position,
+#         'text_color': text_color,
+#         'altered_size': altered_size
+#     }
+
+#     store_json_data(json.dumps(image_data))
+#     return {"message": "Data saved to history.json"}
+
+# def generate_html(history_data):
+#     html = """
+#     <html>
+#     <head>
+#     <style>
+#     table {
+#         border-collapse: collapse;
+#         width: 100%;
+#     }
+    
+#     th, td {
+#         text-align: left;
+#         padding: 8px;
+#         border-bottom: 1px solid #ddd;
+#     }
+#     </style>
+#     </head>
+#     <body>
+    
+#     <h2>History Data</h2>
+    
+#     <table>
+#       <tr>
+#         <th>Image Name</th>
+#         <th>Font Size</th>
+#         <th>Position</th>
+#         <th>Text Color</th>
+#         <th>Altered Size</th>
+#       </tr>
+#     """
+
+#     for data in history_data:
+#         image_name = data['image_name']
+#         font_size = data['font_size']
+#         position = data['position']
+#         text_color = data.get('text_color', 'N/A')
+#         altered_size = data.get('altered_size', 'N/A')
+        
+#         html += f"""
+#         <tr>
+#           <td>{image_name}</td>
+#           <td>{font_size}</td>
+#           <td>{position}</td>
+#           <td>{text_color}</td>
+#           <td>{altered_size}</td>
+#         </tr>
+#         """
+
+#     html += """
+#     </table>
+    
+#     </body>
+#     </html>
+#     """
+    
+#     return html
+
+# def main():
+#     st.title('Image Overlay App')
+#     st.subheader('Upload Images')
+
+#     uploaded_files = st.file_uploader('Choose images', accept_multiple_files=True)
+
+#     if uploaded_files:
+#         image_data_list = []
+#         st.subheader('Image Details')
+
+#         for i, uploaded_file in enumerate(uploaded_files):
+#             st.write(f"**Image {i+1}:**")
+#             image_name = st.text_input(f"Enter name for Image {i+1}", f"Image {i+1}")
+#             font_size = st.number_input(f"Enter font size for Image {i+1}", min_value=1, max_value=200, value=30)
+#             position = st.selectbox(f"Select position for Image {i+1}", ('top-left', 'bottom-left', 'bottom-right'))
+#             text_color = st.color_picker(f"Select text color for Image {i+1}", '#000000')
+#             altered_size = st.checkbox(f"Image {i+1} size altered")
+
+#             image_data = {
+#                 'image_name': image_name,
+#                 'font_size': font_size,
+#                 'position': position,
+#                 'text_color': text_color,
+#                 'altered_size': altered_size
+#             }
+
+#             image_data_list.append(image_data)
+
+#         st.subheader('Preview')
+#         images = [uploaded_file.read() for uploaded_file in uploaded_files]
+#         images_with_overlay = add_image_overlay(images, image_data_list)
+
+#         for image_with_overlay in images_with_overlay:
+#             st.image(image_with_overlay)
+
+#         if st.button('Process and Save'):
+#             response = save_to_history(image_data_list)
+#             st.success(response['message'])
+
+#     st.subheader('History')
+#     history_data = load_json_data()
+#     history_html = generate_html(history_data)
+#     st.write(history_html)
+
+# if __name__ == '__main__':
+#     main()
+
+
 import streamlit as st
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
 from PIL import Image, ImageDraw, ImageFont
 import io
 import json
 import base64
+
+app = Flask(__name__)
+api = Api(app)
+
+class ImageOverlay(Resource):
+    def post(self):
+        uploaded_files = request.files.getlist('images')
+        image_data_list = []
+
+        for i, uploaded_file in enumerate(uploaded_files):
+            image_name = request.form.get(f'image_name_{i+1}')
+            font_size = int(request.form.get(f'font_size_{i+1}'))
+            position = request.form.get(f'position_{i+1}')
+
+            image_data_list.append({
+                'image_name': image_name,
+                'font_size': font_size,
+                'position': position
+            })
+
+        images = [uploaded_file.read() for uploaded_file in uploaded_files]
+        images_with_overlay = add_image_overlay(images, image_data_list)
+
+        response_data = []
+        for i, image_with_overlay in enumerate(images_with_overlay):
+            img_byte_arr = io.BytesIO()
+            image_with_overlay.save(img_byte_arr, format='PNG')
+            img_byte_arr = img_byte_arr.getvalue()
+            img_base64 = base64.b64encode(img_byte_arr).decode('utf-8')
+            response_data.append({'image': img_base64})
+
+        return jsonify(response_data)
+
+class History(Resource):
+    def get(self):
+        history_data = load_json_data()
+        return jsonify(history_data)
+
+    def post(self):
+        data = request.get_json()
+        for item in data:
+            save_to_history(item)
+        return {'message': 'Data saved to history.json'}
+
+api.add_resource(ImageOverlay, '/overlay')
+api.add_resource(History, '/history')
 
 def add_image_overlay(images, image_data_list):
     images_with_overlay = []
@@ -4238,7 +4462,7 @@ def add_image_overlay(images, image_data_list):
         image_name = image_data['image_name']
         font_size = image_data['font_size']
         position = image_data['position']
-        text_color = image_data['text_color']
+        font_color = image_data.get('font_color', 'white')
         font = ImageFont.truetype('arial.ttf', font_size)
 
         if position == 'bottom-left':
@@ -4253,20 +4477,23 @@ def add_image_overlay(images, image_data_list):
             y = 10
 
         draw = ImageDraw.Draw(overlay)
-        draw.text((x, y), image_name, font=font, fill=text_color)
+        draw.text((x, y), image_name, font=font, fill=font_color)
 
         img_with_overlay = Image.alpha_composite(img.convert('RGBA'), overlay)
         images_with_overlay.append(img_with_overlay)
 
     return images_with_overlay
 
+
 def resize_image(image, size):
     width, height = size
     return image.resize((width, height), resample=Image.LANCZOS)
 
+
 def store_json_data(json_data):
     with open('history.json', 'a') as file:
         file.write(json_data + '\n')
+
 
 def load_json_data():
     data = []
@@ -4275,23 +4502,25 @@ def load_json_data():
             data.append(json.loads(line))
     return data
 
+
 def save_to_history(data):
     image_name = data['image_name']
     font_size = data['font_size']
     position = data['position']
-    text_color = data['text_color']
+    font_color = data.get('font_color', 'white')
     altered_size = data.get('altered_size', None)
 
     image_data = {
         'image_name': image_name,
         'font_size': font_size,
         'position': position,
-        'text_color': text_color,
+        'font_color': font_color,
         'altered_size': altered_size
     }
 
     store_json_data(json.dumps(image_data))
     return {"message": "Data saved to history.json"}
+
 
 def generate_html(history_data):
     html = """
@@ -4319,7 +4548,7 @@ def generate_html(history_data):
         <th>Image Name</th>
         <th>Font Size</th>
         <th>Position</th>
-        <th>Text Color</th>
+        <th>Font Color</th>
         <th>Altered Size</th>
       </tr>
     """
@@ -4328,15 +4557,15 @@ def generate_html(history_data):
         image_name = data['image_name']
         font_size = data['font_size']
         position = data['position']
-        text_color = data.get('text_color', 'N/A')
-        altered_size = data.get('altered_size', 'N/A')
-        
+        font_color = data.get('font_color', 'white')
+        altered_size = data['altered_size'] if 'altered_size' in data else 'Default'
+
         html += f"""
         <tr>
           <td>{image_name}</td>
           <td>{font_size}</td>
           <td>{position}</td>
-          <td>{text_color}</td>
+          <td>{font_color}</td>
           <td>{altered_size}</td>
         </tr>
         """
@@ -4350,49 +4579,68 @@ def generate_html(history_data):
     
     return html
 
+
 def main():
-    st.title('Image Overlay App')
-    st.subheader('Upload Images')
+    st.title('Image Overlay API')
 
-    uploaded_files = st.file_uploader('Choose images', accept_multiple_files=True)
-
+    uploaded_files = st.file_uploader('Upload images', type=['png', 'jpg', 'jpeg'], accept_multiple_files=True)
     if uploaded_files:
         image_data_list = []
-        st.subheader('Image Details')
+        images = [uploaded_file.read() for uploaded_file in uploaded_files]
 
         for i, uploaded_file in enumerate(uploaded_files):
-            st.write(f"**Image {i+1}:**")
-            image_name = st.text_input(f"Enter name for Image {i+1}", f"Image {i+1}")
-            font_size = st.number_input(f"Enter font size for Image {i+1}", min_value=1, max_value=200, value=30)
-            position = st.selectbox(f"Select position for Image {i+1}", ('top-left', 'bottom-left', 'bottom-right'))
-            text_color = st.color_picker(f"Select text color for Image {i+1}", '#000000')
-            altered_size = st.checkbox(f"Image {i+1} size altered")
+            image_name = st.text_input(f'Enter image name for Image {i+1}', value=uploaded_file.name)
+            font_size = st.number_input(f'Overlay font size for Image {i+1}', min_value=1, value=20)
+            position = st.selectbox(f'Overlay position for Image {i+1}', ('bottom-left', 'bottom-right', 'top-left', 'top-right'))
 
-            image_data = {
+            image_data_list.append({
                 'image_name': image_name,
                 'font_size': font_size,
-                'position': position,
-                'text_color': text_color,
-                'altered_size': altered_size
-            }
+                'position': position
+            })
 
-            image_data_list.append(image_data)
-
-        st.subheader('Preview')
-        images = [uploaded_file.read() for uploaded_file in uploaded_files]
         images_with_overlay = add_image_overlay(images, image_data_list)
 
-        for image_with_overlay in images_with_overlay:
-            st.image(image_with_overlay)
+        for i, image_with_overlay in enumerate(images_with_overlay):
+            st.image(image_with_overlay, caption=f'Image {i+1} with Overlay', use_column_width=True)
 
-        if st.button('Process and Save'):
-            response = save_to_history(image_data_list)
-            st.success(response['message'])
+            if st.checkbox(f"Resize Image {i+1}"):
+                unique_key = f"resize_select_{i}"
+                image_size = st.selectbox("Select Image Size", (
+                    "Default", "1920x1080", "630x900", "720x1080", "900x1260",
+                    "1080x1440", "1440x1800", "1530x1980", "1800x2520", "1050x1500",
+                    "1200x1800", "1500x2100", "1800x2400", "2400x3000", "2550x3300",
+                    "2800x3920", "3025x3850"
+                ), key=unique_key)
 
-    st.subheader('History')
-    history_data = load_json_data()
-    history_html = generate_html(history_data)
-    st.write(history_html)
+                if image_size == "Default":
+                    resized_image_with_overlay = image_with_overlay
+                    altered_size = "Default"
+                else:
+                    width, height = map(int, image_size.split("x"))
+                    resized_image_with_overlay = resize_image(image_with_overlay, (width, height))
+                    altered_size = image_size
+
+                st.image(resized_image_with_overlay, caption=f"Resized Image {i+1}", use_column_width=True)
+
+                image_data_list[i]['altered_size'] = altered_size
+
+        if st.button('Save to History'):
+            for data in image_data_list:
+                save_to_history(data)
+            st.success('Data saved to history.json')
+
+    if st.button('View History'):
+        history_data = load_json_data()
+        history_html = generate_html(history_data)
+        st.write(history_html, unsafe_allow_html=True)
+
+    if st.button('Download History HTML'):
+        history_data = load_json_data()
+        history_html = generate_html(history_data)
+        b64 = base64.b64encode(history_html.encode()).decode()
+        href = f'<a href="data:text/html;base64,{b64}" download="history.html">Download history.html File</a>'
+        st.markdown(href, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
